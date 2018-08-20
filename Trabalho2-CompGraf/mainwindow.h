7af7d39668b24d73eac345070ce375318fbda18d
Tomaz Canabrava <tcanabrava@kde.org>
2018-08-20 11:33:27 +0200

    Separacao em funcoes.
    Uma funcao pra verificar se esta dentro da reta, que retorna o
    ponto de intersessao
    
    uma funcao pra ordenar os pontos de menor pra maior
    
    renomear a funcao location pra checkLocation.
    
    Todos os parametros estao sendo passados por referencia constante
    porque nao queremos alterar os parametros.
    
    Removidas os warnings de compilacao.

--- Trabalho2-CompGraf/mainwindow.h
+++ Trabalho2-CompGraf/mainwindow.h
@@ -18,8 +18,22 @@ public:
     void mousePressEvent(QMouseEvent *e) override;
     void paintGL() override;
     void initializeGL() override;
-    void resizeGL(int width, int height);
-    void location(QPointF pontoDeTeste);
+    void resizeGL(int width, int height) override;
+
+    // funcoes de geometria.
+
+    /* orderPoints
+     * Retorna um std::pair com o membro first sendo o maior ponto e second sendo o menor.
+     */
+    std::pair<QPointF, QPointF> orderPoints(const QPointF& p1, const QPointF& p2) const;
+
+    /* retorna o ponto de intersessao de testPoint com a reta (p1, p2) ou NAN caso nao exista intersessao.
+     */
+    double intersectionY(const QPointF& p1, const QPointF& p2, const QPointF& testPoint) const;
+
+    /* checa o ponto de teste em relacao a todas as retas do poligono.
+     * e guarda em pontosDentro ou pontosFora */
+    void checkLocation(const QPointF& pontoDeTeste);
 private:
     std::vector<QPointF> pontosDoPoligono;
     std::vector<QPointF> pontosDentro;
